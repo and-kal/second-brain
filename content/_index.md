8aa2833103a498212483d0598fced4b0f6bcf026
and-kal <andre@grrlz.net>
2025-01-19 10:40:38 +0100

    hide reading time

--- content/_index.md
+++ content/_index.md
@@ -3,6 +3,10 @@ title: "Limited Entertainment"
 enableToc: false
 ---
 
+<!--
+"Hello! this here is my blog / second brain / digital garden / whatchamacallit. No comment functionality. But I love to chat. Send me a DM on Mastodon: @staxl@sonomu.club Or write to me via Matrix: @staxl:matrix.org."
+-->
+
 ## /dir
 
 1. [/books](/books) — book excerpts, reviews and personal reading notes
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -19,7 +19,7 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+    Component.ContentMeta({ showReadingTime: false }),
     Component.TagList(),
   ],
   left: [
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta({ showReadingTime: false })],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
